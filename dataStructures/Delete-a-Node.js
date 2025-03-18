8fa395b1714231498f7b5d0593e63e4f5396a948
pini91 <brenda.loncaric@gmail.com>
2025-03-18 13:20:27 -0400

    Adding new hackerRank challenge.

---
+++ dataStructures/Delete-a-Node.js
@@ -0,0 +1,69 @@
+// Delete the node at a given position in a linked list and return a reference to the head node. The head is at position 0. The list may be empty after you delete the node. In that case, return a null value.
+
+// Example
+
+
+
+// After removing the node at position , .
+
+// Function Description
+
+// Complete the deleteNode function in the editor below.
+
+// deleteNode has the following parameters:
+// - SinglyLinkedListNode pointer llist: a reference to the head node in the list
+// - int position: the position of the node to remove
+
+// Returns
+// - SinglyLinkedListNode pointer: a reference to the head of the modified list
+
+// Input Format
+
+// The first line of input contains an integer , the number of elements in the linked list.
+// Each of the next  lines contains an integer, the node data values in order.
+// The last line contains an integer, , the position of the node to delete.
+
+// Constraints
+
+// , where  is the  element of the linked list.
+// Sample Input
+
+// 8
+// 20
+// 6
+// 2
+// 19
+// 7
+// 4
+// 15
+// 9
+// 3
+// Sample Output
+
+// 20 6 2 7 4 15 9
+// Explanation
+
+// The original list is . After deleting the node at position , the list is .
+
+function deleteNode(llist, position) {
+    if(!llist){
+        return null
+    }
+
+    if(position===0){
+        return llist.next
+    }
+
+    let current = llist
+
+    for(let i=0; i< position-1;i++){
+        if(!current.next){ //if the list is shorter than the given position
+            return llist
+        }
+
+        current= current.next
+    }
+
+    current.next= current.next.next
+    return llist
+}
