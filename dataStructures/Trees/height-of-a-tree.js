20b796e2e93d9e410319e8c88f789fc681821453
pini91 <brenda.loncaric@gmail.com>
2025-03-31 15:25:33 -0400

    Adding new hackerRank challenge.

---
+++ dataStructures/Trees/height-of-a-tree.js
@@ -0,0 +1,71 @@
+// The height of a binary tree is the number of edges between the tree's root and its furthest leaf. For example, the following binary tree is of height :
+
+// image
+// Function Description
+
+// Complete the getHeight or height function in the editor. It must return the height of a binary tree as an integer.
+
+// getHeight or height has the following parameter(s):
+
+// root: a reference to the root of a binary tree.
+// Note -The Height of binary tree with single node is taken as zero.
+
+// Input Format
+
+// The first line contains an integer , the number of nodes in the tree.
+// Next line contains  space separated integer where th integer denotes node[i].data.
+
+// Note: Node values are inserted into a binary search tree before a reference to the tree's root node is passed to your function. In a binary search tree, all nodes on the left branch of a node are less than the node value. All values on the right branch are greater than the node value.
+
+// Constraints
+
+
+
+// Output Format
+
+// Your function should return a single integer denoting the height of the binary tree.
+
+// Sample Input
+
+// image
+
+// Sample Output
+
+// 3
+// Explanation
+
+// The longest root-to-leaf path is shown below:
+
+// image
+
+// There are  nodes in this path that are connected by  edges, meaning our binary tree's .
+
+/*
+    Node is defined as
+    var Node = function(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+*/
+
+// This is a "method-only" submission.
+// You only need to complete this method.
+
+function treeHeight(root) {
+
+    if(!root|| !root.left && !root.right){
+        return 0
+    }
+
+    let left = 0
+    let right=0
+
+    left= treeHeight(root.left)
+    right= treeHeight(root.right)
+
+    return Math.max(left, right)+1
+
+}
+
+
